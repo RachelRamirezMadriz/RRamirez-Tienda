3b8e2dfb608526e6264bd7f05c4b72e4e7ebc81f
RachelRamirezMadriz <rramirez80534@ufide.ac.cr>
2025-10-23 15:32:52 -0600

    Commit Semana 5

---
+++ tienda/src/main/java/com/tienda/demo/repository/ProductoRepository.java
@@ -0,0 +1,10 @@
+package com.tienda.demo.repository;
+
+import com.tienda.demo.domain.Producto;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductoRepository extends JpaRepository<Producto,Integer>{
+    public List<Producto> findByActivoTrue();
+
+}
